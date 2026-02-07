52c52eabf03882b6bbfaf8c17800a4c6b420ffd5
teplostanski <igor@teplostanski.me>
2026-02-07 23:09:31 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.')
   
   return {
-    base: env.VITE_BASE_URL,
+    base: env.VITE_BASE_URL || '/',
     server: {
       port: 8080,
       open: true
